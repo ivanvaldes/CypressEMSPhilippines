f6ec19f6d97d9aded1cb550dd97f85c2e834d7aa
Ivan Valdes <ivmigliore@gmail.com>
2021-09-27 16:21:01 -0500

    feat: Adding BaseURL

--- cypress/integration/initial-setup/initial-setup.spec.js
+++ cypress/integration/initial-setup/initial-setup.spec.js
@@ -1,6 +1,6 @@
 describe('Login / Logout Test', () => {
 	it('Login', () => {
-		cy.visit('https://127.0.0.1:8444/ssp/')
+		cy.visit(Cypress.config().baseUrl)
 		cy.get('.gwt-InlineLabel').contains('Welcome')
 	})
 })
