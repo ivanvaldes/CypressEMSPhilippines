de00895baf24fc7352066872babf724a1779f8a9
Ivan Valdes <ivmigliore@gmail.com>
2021-10-07 18:02:25 -0500

    refactor: Login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,11 @@
 import 'cypress-file-upload';
 
-Cypress.Commands.add('login', (username, password) => {
+Cypress.Commands.add('login', (url, username, password) => {
+	//Go to page
+	cy.visit(url)
+	cy.get('.gwt-InlineLabel').contains('Welcome', {timeout: 6000})
+
+	//Fill up information
     cy.get('input[name="j_username"]').type(username)
 	cy.get('input[name="j_password"]').type(password)
 	cy.get('#login-submit-button').click()
